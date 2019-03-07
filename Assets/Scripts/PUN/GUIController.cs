b58d715355d4abea1b3e2b79b309cfd11874a3e7
Piccolo <nelsonsulter91@gmail.com>
2019-03-07 23:10:02 +0100

    Connexion cloud et création room - Une scène de test est créée on se connecte au cloud de photon et tente de rejoindre une room, dans le cas où aucune room n'est créée on en fait une et on la rejoin.

---
+++ Assets/Scripts/PUN/GUIController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUIController : MonoBehaviour
+{
+    Text statusText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        statusText = GameObject.Find("StatusText").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        statusText.text = "Status : " + PhotonNetwork.connectionStateDetailed.ToString();
+    }
+}
---
+++ Assets/Scripts/PUN/NetworkController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkController : MonoBehaviour
+{
+
+    private String _gameVersion = "0.1";
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Connexion au cloud Photon
+        PhotonNetwork.ConnectUsingSettings(_gameVersion);
+    }
+
+    // Si et seulement si on a le callback "je viens de rejoindre le lobby", alors on permet de rejoindre une random room
+    //Cette méthode est propre à photon
+    void OnJoinedLobby()
+    {
+        
+        //commande pour rejoindre une room random;
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    void OnPhotonRandomJoinFailed()
+    {
+        Debug.Log("Impossible de rejoindre une random room -> Création d'une room.");
+        PhotonNetwork.CreateRoom(null);
+    }
+}
