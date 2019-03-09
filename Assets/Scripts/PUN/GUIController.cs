ec74b40851a78c1d77808f9f0f17b3297e9f5cc4
Piccolo <nelsonsulter91@gmail.com>
2019-03-09 01:10:57 +0100

    Multijoueur Plusieurs personnages - Plusieurs personnages apparaissent à l'écran et bougent indépendemment les uns des autres ! cependant l'inventaire d'équipement bug, des réglages sont à faire, les images ne disparaissent pas quand on se dbarasse d'un objet et les buffs ne s'en vont pas.

--- Assets/Scripts/PUN/GUIController.cs
+++ Assets/Scripts/PUN/GUIController.cs
@@ -1,21 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class GUIController : MonoBehaviour
+namespace PUN
 {
-    Text statusText;
+    public class GUIController : MonoBehaviour
+    {
+        Text statusText;
+        Text isMaster;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        statusText = GameObject.Find("StatusText").GetComponent<Text>();
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
+            statusText = GameObject.Find("StatusText").GetComponent<Text>();
+            isMaster = GameObject.Find("IsMaster").GetComponent<Text>();
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        statusText.text = "Status : " + PhotonNetwork.connectionStateDetailed.ToString();
+        // Update is called once per frame
+        void Update()
+        {
+            statusText.text = "Status : " + PhotonNetwork.connectionStateDetailed.ToString();
+            isMaster.text = "is master client: " + PhotonNetwork.isMasterClient;
+        }
     }
 }
--- Assets/Scripts/PUN/NetworkController.cs
+++ Assets/Scripts/PUN/NetworkController.cs
@@ -1,31 +1,46 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class NetworkController : MonoBehaviour
+namespace PUN
 {
-
-    private String _gameVersion = "0.1";
-    // Start is called before the first frame update
-    void Start()
+    public class NetworkController : MonoBehaviour
     {
-        //Connexion au cloud Photon
-        PhotonNetwork.ConnectUsingSettings(_gameVersion);
-    }
 
-    // Si et seulement si on a le callback "je viens de rejoindre le lobby", alors on permet de rejoindre une random room
-    //Cette méthode est propre à photon
-    void OnJoinedLobby()
-    {
+        public GameObject playerGameObject;
+
+        private String _gameVersion = "0.1";
+        // Start is called before the first frame update
+        void Start()
+        {
+            //Connexion au cloud Photon
+            PhotonNetwork.ConnectUsingSettings(_gameVersion);
+        }
+
+        // Si et seulement si on a le callback "je viens de rejoindre le lobby", alors on permet de rejoindre une random room
+        //Cette méthode est propre à photon
+        void OnJoinedLobby()
+        {
+            
+            Debug.Log("On tente de rejoindre une room aléatoire.");
         
-        //commande pour rejoindre une room random;
-        PhotonNetwork.JoinRandomRoom();
-    }
+            //commande pour rejoindre une room random;
+            PhotonNetwork.JoinRandomRoom();
+        }
 
-    void OnPhotonRandomJoinFailed()
-    {
-        Debug.Log("Impossible de rejoindre une random room -> Création d'une room.");
-        PhotonNetwork.CreateRoom(null);
+        void OnPhotonRandomJoinFailed()
+        //Le callback dans le cas où on arrive pas à rejoindre de room au hasard, on en crée une.
+        {
+            Debug.Log("Impossible de rejoindre une random room -> Création d'une room.");
+            PhotonNetwork.CreateRoom(null);
+        }
+
+        void OnJoinedRoom()
+        //Une fois qu'on est dans une room, on instancie le joueur de manière à ce que tout le monde puisse le voir.
+        {
+            Debug.Log("On vient de rejoindre une room");
+            // On instancie le joueur, en récupérant le prefab, sa position et sa rotation
+            PhotonNetwork.Instantiate("Prefabs/" + playerGameObject.name, playerGameObject.transform.position,
+                Quaternion.identity, 0);
+        }
     }
 }
